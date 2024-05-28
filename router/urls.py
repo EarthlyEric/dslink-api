b7d7fde33e2b77b3f0439b7f1b795205d0e73218
EarthlyEric <earthlyeric6@gmail.com>
2024-05-28 11:40:10 +0800

    chore: Update URL redirection logic and add Redis caching

--- router/urls.py
+++ router/urls.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from fastapi.exceptions import HTTPException
+from fastapi.responses import RedirectResponse
 from app import database,redisClient
 
 urls = APIRouter()
@@ -7,13 +8,14 @@ urls = APIRouter()
 @urls.get("/{hash}")
 async def redirect_to(hash:str):
     url = await redisClient.get(hash)
+    url = url.decode('utf-8') if url else None
     if url is None:
-        url_entry = await database.urls.find_one({"hash": hash})
+        url_entry = await database.links.find_one({"hash": hash})
         if url_entry:
             redisResult = await redisClient.set(hash, str(url_entry["url"]))
-            return {"url": url_entry["url"]}
+            return RedirectResponse(url=url_entry["url"],status_code=301)
     else:
-        return {"url": url}
+        return RedirectResponse(url=url,status_code=301)
     
     raise HTTPException(status_code=404, detail="Short URL not found")
 
