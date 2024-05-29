879c1363eaedc16383760695c926bc0d1d3da38b
EarthlyEric <earthlyeric6@gmail.com>
2024-05-29 16:59:18 +0800

    chore: Update URL redirection logic and improve Redis caching

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import pymongo
 import dns.resolver
 import redis.asyncio as redis
-from motor.motor_asyncio import AsyncIOMotorClient
+import logging
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from core.config import Config
+from motor.motor_asyncio import AsyncIOMotorClient
 
 config = Config()
 
@@ -15,9 +17,7 @@ origins = [
 
 app = FastAPI(title="DSLink API",
             version=config.version,
-            description="A RESTful API for DSLink",
-            docs_url="/docs",
-            redoc_url=None
+            description="A RESTful API for DSLink"
             )
 
 app.mount("/static", StaticFiles(directory="web/static",html=True), name="static")
@@ -26,19 +26,39 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],  # Adjust the allowed methods as needed
-    allow_headers=["*"],  # You can adjust this according to your requirements
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  
+    allow_headers=["*"],  
 )
-
 dns.resolver.default_resolver=dns.resolver.Resolver(configure=False)
 dns.resolver.default_resolver.nameservers=['8.8.8.8']
 
 databaseClient = AsyncIOMotorClient(config.mongodb_uri)
 database = databaseClient[config.mongodb_db]
-
-
-
 redisClient = redis.Redis.from_url(config.redis_uri)
+logging.info("Database connection established")
+
+async def checkIndex():   
+    indexes_cursor = database.links.list_indexes()
+    indexes = await indexes_cursor.to_list(length=None)  # Await the coroutine and convert the cursor to a list
+    index_names = [index["name"] for index in indexes]
+    if not "expiration_time" in index_names:
+        await database.links.create_index([("expiration_time", pymongo.ASCENDING)], expireAfterSeconds=0)
+        logging.info("Index created")
+    logging.info("Database connection established")
+
+
+async def closeDB():
+    databaseClient.close()
+    redisClient.close()
+    logging.info("Database connection closed")
+
+@app.on_event("startup")
+async def startup_event():
+    await checkIndex()
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await closeDB()
 
 from router.urls import urls
 from router.api import api
