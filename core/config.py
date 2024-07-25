08f397e59e42fe92a4df9d5a2b60e362facf3d1a
EarthlyEric6 <earthlyeric6@gmail.com>
2024-07-26 02:05:07 +0800

    Update config.py

--- core/config.py
+++ core/config.py
@@ -10,15 +10,7 @@ class Config:
             print("Config file not found ! Please edit a config.example.json file in the root directory")
             exit(1)
         self.version = str(self.config['version'])
-
-        self.mongodb_uri = os.getenv('MONGODB_URI')
-        self.mongodb_db = os.getenv('MONGODB_DB')
-        self.redis_uri = os.getenv('REDIS_URI')
-        
-        if self.mongodb_uri is None:
-            self.mongodb_uri = self.config['connections']['mongoDB']['uri']
-        if self.mongodb_db is None:
-            self.mongodb_db = self.config['connections']['mongoDB']['database']
-        if self.redis_uri is None:
-            self.redis_uri = self.config['connections']['redis']['uri']
+        self.mongodb_uri = self.config['connections']['mongoDB']['uri']
+        self.mongodb_db = self.config['connections']['mongoDB']['database']
+        self.redis_uri = self.config['connections']['redis']['uri']
        
