719544e40787f2d390b05e9d824756bc87cc7df6
EarthlyEric <earthlyeric6@gmail.com>
2024-07-30 23:51:49 +0800

    resolve commit

--- core/config.py
+++ core/config.py
@@ -10,23 +10,8 @@ class Config:
             print("Config file not found ! Please edit a config.example.json file in the root directory")
             exit(1)
         self.version = str(self.config['version'])
-<<<<<<< HEAD
-
-        self.mongodb_uri = os.getenv('MONGODB_URI')
-        self.mongodb_db = str(os.getenv('MONGODB_DB'))
-        self.redis_uri = str(os.getenv('REDIS_URI'))
-        self.buildid = str(os.getenv('BUILD_ID'))
-        
-        if self.mongodb_uri is None:
-            self.mongodb_uri = self.config['connections']['mongodb']['uri']
-        if self.mongodb_db is None:
-            self.mongodb_db = self.config['connections']['mongodb']['database']
-        if self.redis_uri is None:
-            self.redis_uri = self.config['connections']['redis']['uri']
-       
-=======
         self.mongodb_uri = self.config['connections']['mongoDB']['uri']
         self.mongodb_db = self.config['connections']['mongoDB']['database']
         self.redis_uri = self.config['connections']['redis']['uri']
+        self.buildid = os.getenv('BUILD_ID')
        
->>>>>>> 08f397e59e42fe92a4df9d5a2b60e362facf3d1a
