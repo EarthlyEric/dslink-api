0186b04445ad57cfc56cadfbba3b2a8544a855a6
EarthlyEric <earthlyeric6@gmail.com>
2024-07-31 22:59:59 +0800

    add statistic usage recorder

--- router/api.py
+++ router/api.py
@@ -24,21 +24,54 @@ async def generate(request:generateSchema):
         "hash": hash,
         "expirydays": expiryDays,
     })
-    redisResult = await redisClient.set(hash, str(request.url),ex=60*60*24*request.expiryDays)
+    await redisClient.set(hash, str(request.url),ex=60*60*24*request.expiryDays)
     if databaseResult.inserted_id:
+        await database.system.update_one({"type":"statistic"},{"$inc":{"route./generate.sucess":1}})
         return {"hash": hash,"expiryDays": request.expiryDays}
     
+    await database.system.update_one({"type":"statistic"},{"$inc":{"route./generate.fail":1}})
     raise HTTPException(status_code=500, detail="Failed to shorten URL")
 
 @api.get("/health")
 async def health():
+    await database.system.update_one({"type":"statistic"},{"$inc":{"route./health":1}})
     return {"status":"ok"}
 
 @api.get("/info")
 async def version():
+    await database.system.update_one({"type":"statistic"},{"$inc":{"route./info":1}})
     return {"name":"DSLink",
             "version":"1.0.0",
             "buildid":config.buildid }
+
+@api.get("/statistic")
+async def statistic():
+    databaseResult = await database.system.find_one({"type":"statistic"})
+    await database.system.update_one({"type":"statistic"},{"$inc":{"route./statistic":1}})
+    return await database.system.insert_one({
+            "type":"statistic",
+            "route":{
+                "api":{
+                    "/generate":{
+                        "sucess":databaseResult["route"]["api"]["/generate"]["sucess"],
+                        "fail":databaseResult["route"]["api"]["/generate"]["fail"]
+                        },
+                    "/lookup":{
+                        "sucess":databaseResult["route"]["api"]["/lookup"]["sucess"],
+                        "fail":databaseResult["route"]["api"]["/lookup"]["fail"]
+                    },
+                    "/health":databaseResult["route"]["api"]["/health"],
+                    "/info":databaseResult["route"]["api"]["/info"],
+                    "/statistic":databaseResult["route"]["api"]["/statistic"]
+                    },
+                "web":{
+                    "redirect":{
+                        "sucess":databaseResult["route"]["web"]["redirect"]["sucess"],
+                        "fail":databaseResult["route"]["web"]["redirect"]["fail"]
+                    }
+                }
+            }
+            })
 
 @api.post("/lookup")
 async def lookup(request:lookupSchema):
@@ -47,11 +80,14 @@ async def lookup(request:lookupSchema):
     if url is None:
         url_entry = await database.links.find_one({"hash": hash})
         if url_entry:
-            redisResult = await redisClient.set(hash, str(url_entry["url"]))
+            await redisClient.set(hash, str(url_entry["url"]))
+            await database.system.update_one({"type":"statistic"},{"$inc":{"route./lookup.sucess":1}})
             return {"url": url_entry["url"]}
     else:
+        await database.system.update_one({"type":"statistic"},{"$inc":{"route./lookup.sucess":1}})
         return {"url": url}
     
+    await database.system.update_one({"type":"statistic"},{"$inc":{"route./lookup.fail":1}})
     raise HTTPException(status_code=404, detail="Short URL not found")
 
 
--- router/urls.py
+++ router/urls.py
@@ -13,10 +13,13 @@ async def redirect_to(hash:str):
         url_entry = await database.links.find_one({"hash": hash})
         if url_entry:
             await redisClient.set(hash, str(url_entry["url"]))
+            await database.system.update_one({"type":"statistic"},{"$inc":{"route.web.redirect.sucess":1}})
             return RedirectResponse(url=url_entry["url"],status_code=301)
     else:
+        await database.system.update_one({"type":"statistic"},{"$inc":{"route.web.redirect.sucess":1}})
         return RedirectResponse(url=url,status_code=301)
     
+    await database.system.update_one({"type":"statistic"},{"$inc":{"route.web.redirect.fail":1}})
     raise HTTPException(status_code=404, detail="Short URL not found")
 
 @urls.get("/")
